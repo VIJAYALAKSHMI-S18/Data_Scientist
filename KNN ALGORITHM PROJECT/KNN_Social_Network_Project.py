da3bc879968e0c573dd340117f3c829cd7831f51
VIJAYALAKSHMI-S18 <svijayalakshmi2107@gmail.com>
2025-12-18 22:25:33 +0530

    File Added

---
+++ KNN ALGORITHM PROJECT/KNN_Social_Network_Project.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+df=pd.read_csv(r'C:\Users\svija\Documents\Data_Scientist\KNN ALGORITHM PROJECT\Social_Network_Ads.csv')
+x=df.drop('Purchased',axis=1)
+y=df['Purchased']
+AI_model=KNeighborsClassifier(n_neighbors=300)
+AI_model.fit(x,y)
+# new_data1=int(input("ENTER AGE:"))
+# new_data2=int(input("ENTER SALARY:"))
+# p=AI_model.predict([[new_data1,new_data2]])
+joblib.dump(AI_model,'Social_Network_Ads_Streamlit.pkl')
+print("Model Saved Successfully!!")
